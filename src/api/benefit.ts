76ae8d142d847f9694bb0c826593de61ea9acd4c
mbudnikm <mbudnikm97@gmail.com>
2019-08-27 15:28:44 +0200

    init commit

---
+++ src/api/benefit.ts
@@ -0,0 +1,20 @@
+import axios from 'axios'
+
+export type Benefit = {
+  "id": string;
+  "beneficiary": {
+    name: string;
+    email: string;
+  };
+  "country": string;
+  "city": string;
+  "service": string;
+  "monthlyFee": number;
+  "subscribedAtDate": string;
+};
+
+
+export const fetchBenefits = () => {
+  return axios.get('http://localhost:3000/benefits')
+    .then(res => res.data)
+}
